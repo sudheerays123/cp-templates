ce953750baf92866f787c07f74b456826e3a8527
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-02-09 11:02:20 +0530

    Create bfs.cpp

---
+++ Graph Algorithms/bfs.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define N 105
+
+vector<ll> adj[N];
+vector<ll> dist(N);
+
+void bfs(ll s){
+
+    vector<bool> visited(N);
+    queue<ll> q;
+
+    visited[s] = true;
+    q.push(s);
+    dist[s] = 0;
+
+    while(!q.empty()){
+
+        ll a = q.front();
+        q.pop();
+
+        // process node a
+
+        for(auto u : adj[a]){
+
+            if(visited[u]) continue;
+            visited[u] = true;
+
+            dist[u] = dist[a] + 1;
+            q.push(u);
+        }
+
+    }
+}
+
+int main() {
+
+    bfs(0);
+
+    return 0;
+}
