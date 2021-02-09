5940c2b76e20a9f64a574dfa3d8a955858f1fae0
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-02-09 11:27:59 +0530

    Update Adjacency Matrix ( Weighted ).cpp

--- Graph Algorithms/Adjacency Matrix ( Weighted ).cpp
+++ Graph Algorithms/Adjacency Matrix ( Weighted ).cpp
@@ -1,27 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define ll long long int
-#define tc ll test;cin >> test;while(test--)
-#define vi vector<ll>
-#define pb push_back
-#define mp make_pair
-#define INF 0x3f3f3f3f3f
-#define MOD 1000000007
-#define ff first
-#define ss second
-#define in >>
-#define out <<
-#define space << " " <<
-#define spacef << " "
-#define fo(i,a,b) for(ll i = a; i <= b; i++)
-#define nextline out "\n"
-#define print(x) for(auto i : x ) cout out i spacef
-#define mmax(x,i) x = max(x,i)
-#define mmin(x,i) x = min(x,i)
-#define N 105
 
 int main() {
 
