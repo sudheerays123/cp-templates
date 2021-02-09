9e51546ff24ee6f2962501ea25bbb851bba803bd
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-02-09 10:38:30 +0530

    Create dfs.cpp

---
+++ Graph Algorithms/dfs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define ll long long int
+#define N 105
+
+vector<bool> visited(N);
+vector<ll> adj[N];
+
+void dfs(ll s){
+    
+    if(visited[s] == true) return;
+    visited[s] = true;
+    
+    // explore s
+    
+    for(auto u : adj[s]){
+        if(visited[u] == false){
+            dfs(u);
+        }
+    }
+}
+
+int main() {
+    
+    dfs(0);
+    
+    return 0;
+}
